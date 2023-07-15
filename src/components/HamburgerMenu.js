4be4461c253f14a6003cd19826d4b708b3d1799e
ghmagno910@gmail.com <ghmagno910@gmail.com>
2023-07-15 09:45:25 -0300

    alterando

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,1 +1,14 @@
 import "./style/botao.css";
+import { useNavigate } from "react-router-dom";
+
+function BotaoLogin() {
+  const navigate = useNavigate();
+  function irParaLogin() {
+    navigate("/login");
+  }
+
+  return (
+      <button className="botao-login"  onClick={irParaLogin}>Login</button>
+  );
+}
+export default BotaoLogin;
