91193c40269e3570b9c6b366b83bc3bacc123265
Fernando Cesar <fernando.csconceicao@outlook.com>
2023-07-17 10:41:25 -0300

    Mudança de cores

--- src/components/barra-lateral/BarraLateral.js
+++ src/components/barra-lateral/BarraLateral.js
@@ -4,41 +4,51 @@ import logoUbuntuStore from "../../assets/img/logo-ubuntu-store.jpeg";
 import { useNavigate } from "react-router-dom";
 
 function BarraLateral() {
-
   const navigate = useNavigate();
 
   function navegar(caminho) {
-
-      navigate(caminho);
-    console.log('teste')
+    navigate(caminho);
+    console.log("teste");
   }
 
   return (
     <div>
       <ul className="barraLateral">
-        <img
-          className="logoEstabelecimento"
-          src={logoUbuntuStore}
-          alt="Logo da empresa UbuntuStore"
-        />
-        <li className="itemBarraLateral" onClick={navegar("/estabelecimentos/home")}>
-          <button className="botaoBarraLateral"onClick={navegar("/estabelecimentos/home")} >
+        <li>
+          <img
+            className="logoEstabelecimento"
+            src={logoUbuntuStore}
+            alt="Logo da empresa UbuntuStore"
+          />
+        </li>
+        
+        <li
+          className="itemBarraLateral"
+          onClick={navegar("/estabelecimentos/home")}
+        >
+          <button
+            className="botaoBarraLateral"
+            onClick={navegar("/estabelecimentos/home")}
+          >
             <img
               className="iconeBarraLateral"
               src={logoUbuntuStore}
               alt="Logo da empresa UbuntuStore"
             />
-            <div className="textoBarraLateral" >Inicio</div>
+            <div className="textoBarraLateral">Inicio</div>
           </button>
         </li>
-        <li className="itemBarraLateral" onClick={navegar("/estabelecimentos/minhaloja")}>
-          <button className="botaoBarraLateral" >
+        <li
+          className="itemBarraLateral"
+          onClick={navegar("/estabelecimentos/minhaloja")}
+        >
+          <button className="botaoBarraLateral">
             <img
               className="iconeBarraLateral"
               src={logoUbuntuStore}
               alt="Logo da empresa UbuntuStore"
             />
-            <div className="textoBarraLateral" >Minha Loja</div>
+            <div className="textoBarraLateral">Minha Loja</div>
           </button>
         </li>
         <li className="itemBarraLateral">
