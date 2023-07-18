7176b032e2bcc3af6da5ca0cf8fd53e5fc423a05
Fernando Cesar <fernando.csconceicao@outlook.com>
2023-07-17 21:14:45 -0300

    icones

--- src/components/barra-lateral/BarraLateral.js
+++ src/components/barra-lateral/BarraLateral.js
@@ -1,19 +1,43 @@
 import "./barra-lateral.css";
 import React from "react";
 import logoUbuntuStore from "../../assets/img/logo-ubuntu-store.jpeg";
-import ItemBarraLateral from "./item-barra-lateral/ItemBarraLateral"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 function BarraLateral() {
+  const navigate = useNavigate();
+  function navegar(caminho) {
+    navigate(caminho);
+  }
 
   return (
     <div id="side">
-      <img id = "logoEstabelecimento"src={logoUbuntuStore}>
-      </img>
+      <img id="logoEstabelecimento" src={logoUbuntuStore}></img>
       <ul className="barraLateral">
-       <ItemBarraLateral caminho="/estabelecimentos/inicio" texto= "Inicio"/>
-       <ItemBarraLateral caminho="/estabelecimentos/minhaloja" texto= "MinhaLoja"/>
+        <div>
+          <li className="itemBarraLateral">
+            <button
+              onClick={navegar("/estabelecimentos/inicio")}
+              className="botaoBarraLateral"
+            >
+              <FontAwesomeIcon className="iconeBarraLateral" icon={faHouse} />
+              <p>Inicio</p>
+            </button>
+          </li>
+        </div>
+        {/* <ItemBarraLateral
+          caminho="/estabelecimentos/minhaloja"
+          texto="MinhaLoja"
+        />
+        <ItemBarraLateral
+          caminho="/estabelecimentos/minhaloja"
+          texto="Cardápio"
+        />
+        <ItemBarraLateral caminho="/estabelecimentos/minhaloja" texto="Conta" />
+        <ItemBarraLateral caminho="/estabelecimentos/minhaloja" texto="Ajuda" /> */}
       </ul>
     </div>
-  )
-  }
+  );
+}
 export default BarraLateral;
