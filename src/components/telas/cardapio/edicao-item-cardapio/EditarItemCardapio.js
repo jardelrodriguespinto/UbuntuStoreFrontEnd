34c184f6c34fbb0704ae75fe0a071c921ca44a37
Fernando Cesar <fernando.csconceicao@outlook.com>
2023-07-21 21:27:57 -0300

    fdsfsd

--- src/components/telas/cardapio/edicao-item-cardapio/EditarItemCardapio.js
+++ src/components/telas/cardapio/edicao-item-cardapio/EditarItemCardapio.js
@@ -14,10 +14,23 @@ function EditarItemCardapio(props) {
   const token = localStorage.getItem("token");
   const estabelecimentoId = useState(localStorage.getItem("estabelecimentoId"));
   const [output,setOutPut] = useState() 
+  const [nome,setNome] = useState() 
+  const [preco,setPreco] = useState() 
+  const [nota,setNota] = useState() 
+  const [descricao,setDescricao] = useState() 
+  const [gluten,setGluten] = useState() 
+  const [vegetariano,setVegetariano] = useState() 
+  const [vegano,setVegano] = useState() 
+  const [calorias,setCalorias] = useState() 
+  const [tempoDePreparo,setTempoDePreparo] = useState() 
+  const [destacado,setDestacado] = useState() 
+  const [disponivel,setDisponivel] = useState() 
+
   useEffect(() => {
     obterTela();
   }, []);
-  const obterTela = async () => {const resposta = await axios.post(
+  const obterTela = async () => {
+    const resposta = await axios.post(
     ubuntuIp.toString() + "/estabelecimento/telas/editarproduto",
     {
       id: estabelecimentoId,
@@ -30,14 +43,36 @@ function EditarItemCardapio(props) {
     }
   );
   console.log(resposta)
+
   setOutPut( resposta.data)
+  setNome( resposta.data.name)
+  setPreco( resposta.price)
+  setNota( resposta.rate)
+  setDescricao( resposta.description)
+  setGluten( resposta.containsGluten)
+  setVegetariano( resposta.vegetarianStamp)
+  setVegano( resposta.veganStamp)
+  setCalorias( resposta.calories)
+  setTempoDePreparo( resposta.cookingTime)
+  setDestacado( resposta.highlighted)
+  setDisponivel( resposta.disponivel)
   }
   return (
     <div>
       <HeaderEstabelecimento logo={true} />
       <div className="homeEstabelecimento">
         <BarraLateral />
-        <main>{JSON.stringify(output)}</main>
+        <main>
+          <form>
+            <label>Nome</label>
+            <input name="nome" value={nome} onChange={(e)=>{
+              setNome(e.target.value)
+            }}></input>
+          </form>
+
+
+
+        </main>
       </div>
       <Footer />
     </div>
