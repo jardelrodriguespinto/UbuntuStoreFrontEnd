43f258555aa8216607e59c624fcc1667ca4fd8dc
Fernando Cesar <fernando.csconceicao@outlook.com>
2023-08-17 10:36:48 -0300

    bug

--- src/components/telas/minha-loja/MinhaLoja.js
+++ src/components/telas/minha-loja/MinhaLoja.js
@@ -16,13 +16,18 @@ import {
   Typography,
   createTheme,
   Button,
+  Box,
   DialogActions,
+  Dialog,
+  DialogTitle,
+  DialogContent,
 } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+import { DesktopTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { ClockPicker } from "@mui/lab";
 import { parseISO, format } from "date-fns";
+import { response } from "express";
 
 function MinhaLoja() {
   const navigate = useNavigate();
@@ -35,21 +40,24 @@ function MinhaLoja() {
   const [telefone, setTelefone] = useState("");
   const [endereco, setEndereco] = useState("");
   const [cep, setCep] = useState("");
-  const [inicioExpediente, inicioExpedienteSetter] = useState(null);
-  const [fimExpediente] = useState(false);
+  const [openexperienteDialog, setOpenhorarioDialog] = useState("");
+  const [horaInicioExpediente, setHoraInicioExpediente] = useState(6);
+  const [minutosInicioExpediente, setMinutosInicioExpediente] = useState(6);
+  const [horaFimExpediente, setHoraFimExpediente] = useState(6);
+  const [minutosFimExpediente, setMinutosFimExpediente] = useState(6);
+  const [horaInicioExpedienteInicial, setHoraInicioExpedienteInicial] = useState(6);
+  const [minutosInicioExpedienteInicial, setMinutosInicioExpedienteInicial] = useState(6);
+  const [horaFimExpedienteInicial, setHoraFimExpedienteInicial] = useState(6);
+  const [minutosFimExpedienteInicial, setMinutosFimExpedienteInicial] = useState(6);
+
   const [descricao, setDescricao] = useState("");
   const [imagem, setPreviewImage] = useState(null);
+  const [open, setOpen] = useState(false);
   const tema = createTheme(propiedadesDoTema);
 
   const id = localStorage.getItem("estabelecimentoId");
   const enviarFormulario = async () => {
-    let data = new Date(inicioExpediente)
-    let localTimeString= "".concat(data.getHours()).concat(":").concat(data.getMinutes());
 
-    console.log(localTimeString)
-    
-    console.log()
-       let fimE;
     const resposta = await axios.put(
       ubuntuIp.toString() + "/estabelecimento/telas/minhaloja",
       {
@@ -60,9 +68,7 @@ function MinhaLoja() {
         endereco: endereco,
         telefone: telefone,
         cep: cep,
-        valorMinimo: valorMinimo,
-        inicioExpediente: inicioExpediente,
-        fimExpediente: fimExpediente
+        valorMinimo: valorMinimo
       },
       {
         headers: {
@@ -72,26 +78,51 @@ function MinhaLoja() {
     );
   };
 
-  const handleInicioExpedienteChange = (newTime) => {
-    console.log(newTime);// inicioExpedienteSetter(newTime);
-  };
-
-  const setInicioExpedienteInicial = (e) => {
-    {
-      console.log(e)
-      let horas = e.split(":");
-      const dateTime = new Date(1111, 11, 11, horas[0], horas[1], horas[2]);
-
-      inicioExpedienteSetter(dateTime);
-
-      // const parsedDate = parseISO(javaLocalDate);
+  const handleHoraInicioExpedienteChange = (event) => {
+    const newValue = event.target.value;
+    if (newValue >= 0 && newValue <= 24) {
+      setHoraInicioExpediente(newValue);
     }
   };
+  const enviarHorario = async () => {
+    let inicioExpediente = new Date();
+    inicioExpediente.setHours(horaInicioExpediente);
+    inicioExpediente.setMinutes(minutosInicioExpediente);
+    let fimExpediente = new Date();
+    fimExpediente.setHours(horaFimExpediente);
+    fimExpediente.setMinutes(minutosFimExpediente);
+
+    const resposta = await axios.put(
+      ubuntuIp.toString() + "/estabelecimento/alterarhorariofuncionamento",
+      {
+        inicioExpediente: inicioExpediente,
+        fimExpediente: fimExpediente,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    if(resposta.status == 200){
+        setHoraInicioExpedienteInicial(horaInicioExpediente)
+        setMinutosInicioExpedienteInicial(horaInicioExpediente)
+    }
+  };
+  
+
+
 
   useEffect(() => {
     obterTela();
   }, []);
 
+  const handleOpen = () => {
+    setOpenhorarioDialog(true);
+  };
+  const handleClose = () => {
+    setOpenhorarioDialog(false);
+  };
   const obterTela = async () => {
     const resposta = await axios.get(
       ubuntuIp.toString() + "/estabelecimento/telas/minhaloja",
@@ -106,15 +137,99 @@ function MinhaLoja() {
     setValorMinimo(resposta.data.valorMinimo);
     setEndereco(resposta.data.endereco);
     setTelefone(resposta.data.telefone);
-    handleInicioExpedienteChange(resposta.data.inicioExpediente);
-    //1: transformar data do tempo LocalTime
+    // handleInicioExpedienteChange(resposta.data.inicioExpediente);
+    // 1: transformar data do tempo LocalTime
     // setFimExpediente(resposta.data.fimExpediente);
-    setInicioExpedienteInicial(resposta.data.fimExpediente);
+    // setInicioExpediente(resposta.data.fimExpediente);
+    console.log(resposta.data.inicioExpediente)
+    let inicioExpedienteString = resposta.data.inicioExpediente;
+    setHoraInicioExpedienteInicial(resposta.data.inicioExpediente.split(":")[0]);
+    setHoraInicioExpediente(resposta.data.inicioExpediente.split(":")[0]);
+
+    setMinutosInicioExpediente(resposta.data.inicioExpediente.split(":")[1]);
+    setMinutosInicioExpedienteInicial(resposta.data.inicioExpediente.split(":")[1]);
+
+    setHoraFimExpediente(resposta.data.fimExpediente.split(":")[0]);
+    setHoraFimExpedienteInicial(resposta.data.fimExpediente.split(":")[0]);
+    
+    setMinutosFimExpediente(resposta.data.fimExpediente.split(":")[1]);
+    setMinutosFimExpedienteInicial(resposta.data.fimExpediente.split(":")[1]);
+
     setCep(resposta.data.cep);
     setDescricao(resposta.data.descricao);
   };
   return (
     <ThemeProvider theme={tema}>
+      <Dialog open={openexperienteDialog} onClose={handleClose}>
+        <DialogTitle marginBottom={"20px"}>
+          Alterar horario de trabalho
+        </DialogTitle>
+        <DialogContent>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack direction={"column"}>
+              <Stack direction={"row"}>
+                <Box>
+                  <Typography>Hora de Início de Trabalho:</Typography>
+                  <TextField
+                    type="number"
+                    value={horaInicioExpediente}
+                    onChange={(e)=>{
+                      setHoraInicioExpediente(e.target.value)
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Typography>Minutos inicio Trabalho</Typography>
+                  <TextField
+                  type="number"
+                    value={minutosInicioExpediente}
+                    onChange={(e) => {
+                      setMinutosInicioExpediente(e.target.value);
+                    }}
+                  />
+                </Box>
+              </Stack>
+
+              <Stack direction={"row"}>
+                <Box>
+                  <Typography>Hora fim Expediente</Typography>
+                  <TextField
+                  type="number"
+                    value={horaFimExpediente}
+                    onChange={(e) => {
+                      setHoraFimExpediente(e.target.value);
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Typography>Minutos fim Expediente</Typography>
+                  <TextField
+                  type="number"
+                    value={minutosFimExpediente}
+                    onChange={(e) => {
+                      setMinutosFimExpediente(e.target.value);
+                    }}
+                  />
+                </Box>
+              </Stack>
+            </Stack>
+          </LocalizationProvider>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            <Typography textTransform={"none"}>Cancelar</Typography>
+          </Button>
+          <Button
+            onClick={enviarHorario}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            <Typography textTransform={"none"}>Enviar</Typography>
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <HeaderEstabelecimento logo={true} />
       <div className="homeEstabelecimento">
         <BarraLateral />
@@ -203,19 +318,16 @@ function MinhaLoja() {
                   setDescricao(e.target.value);
                 }}
               />
-              <Stack direction={"row"}></Stack>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <TimePicker
-                  value={inicioExpediente}
-                  label="Inicio de Expediente"
-                  onChange={(e) => {
-                    console.log(e)
-                    handleInicioExpedienteChange(e);
-                  }}
-                />
-
-                <TimePicker value={fimExpediente} label="Fim de Expediente" />
-              </LocalizationProvider>
+              <Stack direction={"row"}>
+                <Typography color={"black"}>
+                  {" "}
+                  Horario de trabalho: das {horaInicioExpedienteInicial}:{minutosInicioExpedienteInicial} até as {horaFimExpedienteInicial}:{minutosFimExpedienteInicial}
+                </Typography>
+                <Button onClick={handleOpen}>
+                  <Typography> Editar</Typography>
+                </Button>
+              </Stack>
+              :
             </Stack>
 
             <DialogActions>
